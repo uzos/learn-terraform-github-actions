c2af2697ccf23e4b0eda48c2699f5f9d83268a20
UV <housenos@outlook.com>
2024-01-03 23:16:42 +0000

    update terraform backend in main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+   organization = "UX_HCP"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "github-action-terraform"
     }
   }
 }
